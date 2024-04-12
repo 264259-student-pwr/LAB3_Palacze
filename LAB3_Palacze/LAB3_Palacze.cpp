43950ec1363bb59d90037fd5aa0b3740cb250190
Wojciech Odziomek <264259@student.pwr.edu.pl>
2024-04-12 10:59:02 +0200

    .

--- LAB3_Palacze/LAB3_Palacze.cpp
+++ LAB3_Palacze/LAB3_Palacze.cpp
@@ -2,51 +2,98 @@
 #include <thread>
 #include <mutex>
 #include <semaphore>
+#include <vector>
+#include <cstdlib> // Do czyszczenia konsoli
 
-const int k = 3; // Liczba palaczy
-const int l = 2; // Liczba ubijaczy
-const int m = 3; // Liczba pudełek zapałek
+const int k = 4; // Liczba palaczy
+const int l = 1; // Liczba ubijaczy
+const int m = 1; // Liczba pudełek zapałek
 
 std::mutex screen_mutex; // Semafor do wykluczania dostępu do ekranu
 
-std::semaphore tamper(l); // Semafor dla ubijaczy
-std::semaphore matches(m); // Semafor dla pudełek zapałek
+std::counting_semaphore<l> tamper(l); // Semafor dla ubijaczy
+std::counting_semaphore<m> matches(m); // Semafor dla pudełek zapałek
 
-void smoker(int id) {
+enum ActionType {
+    ACTION_NONE,
+    ACTION_TAMPER,
+    ACTION_LIGHT,
+    ACTION_SMOKE
+};
+
+std::string actionToString(ActionType action) {
+    switch (action) {
+    case ACTION_TAMPER:
+        return "Ubijam ";
+    case ACTION_LIGHT:
+        return "Zapalam";
+    case ACTION_SMOKE:
+        return "Pale   ";
+    default:
+        return "Czekam ";
+    }
+}
+
+void clearConsole() {
+#ifdef _WIN32
+    std::system("cls");
+#endif
+}
+
+void printTable(std::vector<ActionType>& actions) {
+    std::lock_guard<std::mutex> lock(screen_mutex);
+
+    clearConsole(); // Czyszczenie konsoli
+
+    std::cout << "+-------------+-------------+-------------+------------+" << std::endl;
+    std::cout << "| Palacz #1   | Palacz #2   | Palacz #3   | Palacz #4  |" << std::endl;
+    std::cout << "+-------------+-------------+-------------+------------+" << std::endl;
+    std::cout << "| " << actionToString(actions[0]) << "     | " << actionToString(actions[1]) << "     | " << actionToString(actions[2]) << "     | "  << actionToString(actions[3])<< "    |" << std::endl;
+    std::cout << "+-------------+-------------+-------------+----------- +" << std::endl;
+}
+
+void smoker(int id, std::vector<ActionType>& actions) {
     while (true) {
         // Żądanie ubijacza
-        tamper.wait();
+        tamper.acquire();
+
+        actions[id] = ACTION_TAMPER;
+        printTable(actions);
 
         // Ubijanie
-        std::cout << "[Smoker " << id << "] Ubijam fajkę" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(3));
 
         // Zwrot ubijacza
-        tamper.signal();
+        tamper.release();
 
         // Żądanie pudełka zapałek
-        matches.wait();
+        matches.acquire();
+
+        actions[id] = ACTION_LIGHT;
+        printTable(actions);
 
         // Zapalenie fajki
-        std::cout << "[Smoker " << id << "] Zapalam fajkę" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
         // Zwrot pudełka zapałek
-        matches.signal();
+        matches.release();
 
         // Palenie fajki
-        std::cout << "[Smoker " << id << "] Palę fajkę" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        actions[id] = ACTION_SMOKE;
+        printTable(actions);
+        std::this_thread::sleep_for(std::chrono::seconds(4));
 
-        std::cout << "[Smoker " << id << "] Skończyłem palić" << std::endl;
+        actions[id] = ACTION_NONE;
+        printTable(actions);
     }
 }
 
 int main() {
+    std::vector<ActionType> actions(k, ACTION_NONE);
     std::thread smokers[k];
 
     for (int i = 0; i < k; i++) {
-        smokers[i] = std::thread(smoker, i);
+        smokers[i] = std::thread(smoker, i, std::ref(actions));
     }
 
     for (int i = 0; i < k; i++) {
